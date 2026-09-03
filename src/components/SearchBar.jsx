33460577967a48d96fdcf5354806fea702231f28
saiful-aj18 <alexjamson118@gmail.com>
2026-09-03 20:39:08 +0600

    Feat: Add SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+function SearchBar({ searchTerm, setSearchTerm }) {
+  return (
+    <div>
+      <label className="mb-2 block text-sm font-medium text-slate-300">
+        Search Products
+      </label>
+
+      <input
+        type="text"
+        placeholder="Search by product name..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-full rounded-lg border border-slate-700 bg-slate-900 px-4 py-3 text-slate-200 outline-none placeholder:text-slate-500 focus:border-cyan-500"
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
